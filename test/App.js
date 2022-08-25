ada27981c138147e30ee82e500c5fd7029375fee
Wathsala Vishwajith <wathsala-vishwajith@github.com>
2022-08-25 18:02:07 +0530

    Completed the test. Logs the paths.

--- test/App.js
+++ test/App.js
@@ -18,39 +18,16 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const RNFS = require('react-native-fs');
+
+const CachePath = RNFS.CachesDirectoryPath;
+const ExtCachePath = RNFS.ExternalCachesDirectoryPath;
+const DocPath = RNFS.DocumentDirectoryPath;
+const DownPath = RNFS.DownloadDirectoryPath;
+const ExtDirectoryPath = RNFS.ExternalDirectoryPath;
+const ExtStoragePath = RNFS.ExternalStorageDirectoryPath;
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -60,52 +37,23 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.sectionContainer}>
+      <Text>Hello</Text>
+      <Text>CachePath: {CachePath}</Text>
+      <Text>ExtCachePath: {ExtCachePath}</Text>
+      <Text>DocPath: {DocPath}</Text>
+      <Text>DownPath: {DownPath}</Text>
+      <Text>ExtDirectoryPath: {ExtDirectoryPath}</Text>
+      <Text>ExtStoragePath: {ExtStoragePath}</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
